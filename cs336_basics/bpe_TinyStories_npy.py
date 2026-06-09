eb46894249f5402fd5d9a3accbfd071abfc0cc26
flora2627 <c4a4d65b@gmail.com>
2026-06-09 15:03:56 +0800

    backup

--- cs336_basics/bpe_TinyStories_npy.py
+++ cs336_basics/bpe_TinyStories_npy.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     PATH = "./data/my_bpe_tinystories.pkl"
     t0  = time.time()
     tokenizer = bpe_tokenizer.from_file(PATH,["<|endoftext|>"])
-    tokenizer.save_to_npy_file("./data/TinyStoriesV2-GPT4-valid.txt", "./data/my_bpe_tinystories_valid.npy")
+    tokenizer.save_to_npy_file("./data/TinyStoriesV2-GPT4-train.txt", "./data/my_bpe_tinystories_train.npy")
+    # tokenizer.save_to_npy_file("./data/TinyStoriesV2-GPT4-valid.txt", "./data/my_bpe_tinystories_valid.npy")
     print("tokenizer",tokenizer)
     print("time",time.time() - t0)
